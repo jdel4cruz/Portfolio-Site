59ccebb5432f1f1835e3bdc31674f94b2fa75f09
John De La Cruz <john.e.delacruz34@gmail.com>
2022-01-07 22:13:43 -0700

    finished updating skill table. Updated contact form. Added 1024 width media query

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const hamburger = document.querySelector(".hamburger-btn");
 const sidebar = document.querySelector(".sidebar");
 
+const jumpLinks = document.querySelectorAll('a[href^="#"]');
+console.log(jumpLinks);
+// const sections = document.querySelectorAll("section");
+
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("open");
   sidebar.classList.toggle("open");
+});
+
+jumpLinks.forEach((a) => {
+  a.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const section = document.querySelector(a.getAttribute("href"));
+    section.scrollIntoView({ behavior: "smooth" });
+  });
 });
